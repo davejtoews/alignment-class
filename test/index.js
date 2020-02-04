30931a73b158bd5eee4cfba4297fc722d7c184a9
David Toews <davejtoews@gmail.com>
2020-02-04 13:14:29 -0700

    Bundles with webpack

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 require('jsdom-global')();
 var assert = require('chai').assert;
-var AlignmentClass = require('../AlignmentClass.js');
+var AlignmentClass = require('../src/index.js');
 
 describe('AlignmentClass', function() {
 	document.body.innerHTML = '<ul><li>one</li><li>two</li><li>three</li></ul>';
